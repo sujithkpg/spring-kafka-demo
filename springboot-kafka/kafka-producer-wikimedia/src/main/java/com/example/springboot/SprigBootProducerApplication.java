6d2545aa24c6a02114d898bdd7978b5ac3b75133
Sujith Gamage <sujithkpg@gmail.com>
2023-03-19 11:24:08 +0530

    kafka project initialization

---
+++ springboot-kafka/kafka-producer-wikimedia/src/main/java/com/example/springboot/SprigBootProducerApplication.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootProducerApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SprigBootProducerApplication.class);
+    }
+
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
+    }
+}
