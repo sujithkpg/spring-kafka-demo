25b14e0e8913df3e8168b3ba258b6738c1c442fb
Sujith Gamage <sujithkpg@gmail.com>
2023-03-19 14:21:11 +0530

    kafka consumer service changes

---
+++ springboot-kafka/kafka-consumer-database/src/main/java/com/example/springboot/KafkaDatabaseConsumer.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDatabaseConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
+
+    @KafkaListener(
+            topics = "wikimedia_recentchange",
+            groupId = "myGroup"
+    )
+    public void consume(String eventMessage) {
+        LOGGER.info(String.format("Message recieved -> %s", eventMessage));
+
+    }
+}
